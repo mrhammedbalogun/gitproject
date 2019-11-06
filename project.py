8e30e3d4e771b2721d81fd50c1aa018430e273cb
Hammed Olasunkanmi Balogun <mrhammedbalogun@gmail.com>
2019-11-06 16:17:03 +0100

    new practices

--- project.py
+++ project.py
@@ -1,1 +1,54 @@
-2+2
+# x = 20
+# y = 30
+
+# answer = x + y
+
+# print(answer)
+
+# num = 20
+# den = 60
+# pi = den / num
+# print(pi)
+
+# nameTe = "Hammed"
+# print(nameTe)
+
+# phone = 8023900964 * 2
+
+# print(phone)
+
+# firstName = "Hammed"
+# surname = "Balogun"
+# fullname = firstName + ' ' + surname
+
+# print(fullname)
+# day = "10"
+# month = "06"
+# year = "1988"
+# print(day + ' / ' + month + ' / ' + year )
+# pi = 22 / 7
+# statement = "pi is " + str(round(pi,2))
+# print(statement)
+
+# akara = input("Please how many akara do you want: ")
+# akamu = input("Please how many akamu do you want: ")
+
+# akaraPrice = 20
+# akamuPrice = 50
+
+# cost1 = akaraPrice * int(akara)
+
+# cost2 = akamuPrice * int(akamu)
+
+# total = cost1 + cost2
+
+# print("You bill is: ", "#",total)
+
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+
+currentAge = 2019 - int(age)
+
+answer = f"Hello {name} you are {age}, you were born in {currentAge}"
+
+print(answer)
