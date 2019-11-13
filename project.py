c2409d428fd3a3b5996d320dce50db22dc4f98ed
Hammed Olasunkanmi Balogun <mrhammedbalogun@gmail.com>
2019-11-13 10:24:13 +0100

    update class assignment

--- project.py
+++ project.py
@@ -44,11 +44,26 @@
 
 # print("You bill is: ", "#",total)
 
-name = input("Please enter your name: ")
-age = input("Please enter your age: ")
+# name = input("Please enter your name: ")
+# age = input("Please enter your age: ")
 
-currentAge = 2019 - int(age)
+# currentAge = 2019 - int(age)
 
-answer = f"Hello {name} you are {age}, you were born in {currentAge}"
+# answer = f"Hello {name} you are {age}, you were born in {currentAge}"
 
-print(answer)
+# print(answer)
+
+name = input("Please enter your name:")
+age = input("Please enter your age:")
+current_age = 2019 - int(age)
+
+leap_year = [1804,1808,1812,1816,1820,1824,1828,1832,1836,1840,1844,1848,1852,1856,1860,1864,1868,1872,1876,1880,1884,1888,1892,1896,1904,1908,1912,1916,1920,1924,1928,1932,
+1936,1940,1944,1948,1952,1956,1960,1964,1968,1972,1976,1980,1984,1988,1992,1996,2000,2004,2008,2012,2016]
+
+for i in leap_year:
+    if i == current_age:
+        print(name,",", "Youre born in a leap year")
+        break
+    else:
+        print(name,",", " Youre not born in a leap year")
+        break
